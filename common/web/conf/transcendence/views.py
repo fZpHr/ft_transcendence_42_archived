52638ecb22785e49070b7a8ffbbcb16aa02c0f8a
Ezuker <benoit.carolle.pro@gmail.com>
2024-08-07 22:10:23 -0400

    fix: solved png file unfindable, fix navbar when disconnected

--- common/web/conf/transcendence/views.py
+++ common/web/conf/transcendence/views.py
@@ -14,15 +14,21 @@ def set_language(request, language_code):
         request.session['django_language'] = language_code
         activate(language_code)
         return redirect(next_page)
-    
-
     return redirect(reverse('some_default_view')) 
 
 def custom_404_view(request, exception):
     return render(request, 'global_templates/404.html', {}, status=404)
 
-
 def index(request):
+    print(request.user)
+    if request.user.is_authenticated:
+        context = {
+            'no_footer': False,
+        }
+    else:
+        context = {
+            'no_footer': True,
+        }
     if request.htmx:
-        return render(request, 'homePage.html', {})
-    return render(request, 'homePage_full.html', {})
+        return render(request, 'homePage.html', context)
+    return render(request, 'homePage_full.html', context)
