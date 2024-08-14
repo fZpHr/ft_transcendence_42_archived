c0e96c6bc9a6d70fec694b7a50483ef43c22907c
Ezuker <benoit.carolle.pro@gmail.com>
2024-08-14 19:08:06 -0400

    feat: Now icon are used to go directly into ranked

---
+++ common/web/conf/game/static/home/js/home.js
@@ -0,0 +1,37 @@
+import { createSocket } from "../../ranked/js/socket.js";
+
+document.getElementById("pong-ranked-btn").addEventListener("click", async function() {
+    console.log("pong-button clicked");
+    let intervalId = setInterval(() => {
+        let divConnect4 = document.getElementById("wrap");
+        if (divConnect4) {
+            clearInterval(intervalId);
+            createSocket("pong");
+            const playerDiv = document.getElementById("player-btn");
+            const opponentDiv = document.getElementById("opps-btn");
+            const gameDiv = document.getElementById("game-type");
+            const vsDiv = document.getElementById("vs-text");
+            const waitingDiv = document.getElementById("waiting-btn");
+            [playerDiv, opponentDiv, gameDiv, vsDiv].forEach(el => el.style.display = "none");
+            [divConnect4, waitingDiv].forEach(el => el.style.display = "flex");
+        }
+    }, 100);
+});
+
+document.getElementById("connect4-ranked-btn").addEventListener("click", async function() {
+    console.log("connect4-button clicked");
+    let intervalId = setInterval(() => {
+        let divConnect4 = document.getElementById("wrap");
+        if (divConnect4) {
+            clearInterval(intervalId);
+            createSocket("connect4");
+            const playerDiv = document.getElementById("player-btn");
+            const opponentDiv = document.getElementById("opps-btn");
+            const gameDiv = document.getElementById("game-type");
+            const vsDiv = document.getElementById("vs-text");
+            const waitingDiv = document.getElementById("waiting-btn");
+            [playerDiv, opponentDiv, gameDiv, vsDiv].forEach(el => el.style.display = "none");
+            [divConnect4, waitingDiv].forEach(el => el.style.display = "flex");
+        }
+    }, 100);
+  });
--- common/web/conf/game/static/ranked/js/socket.js
+++ common/web/conf/game/static/ranked/js/socket.js
@@ -61,83 +61,54 @@ function createSocket(gameType) {
     }
   }, 3000);
 
+  document.getElementById("reconnect").addEventListener("click", function() {
+    console.log("reconnect clicked");
+    createSocket(gameType);
+    const playerDiv = document.getElementById("player-btn");
+    const opponentDiv = document.getElementById("opps-btn");
+    const gameDiv = document.getElementById("game-type");
+    const vsDiv = document.getElementById("vs-text");
+    const waitingDiv = document.getElementById("waiting-btn");
+    const divOpponentDisconnected = document.getElementById("overlay");
+    const divConnect4 = document.getElementById("wrap");
+    [playerDiv, opponentDiv, gameDiv, vsDiv, divOpponentDisconnected].forEach(el => el.style.display = "none");
+    [divConnect4, waitingDiv].forEach(el => el.style.display = "flex");
+  });
+  
+  document.getElementById("cancel").addEventListener("click", function() {
+    console.log("cancel clicked");
+    let divWaiting = document.getElementById("game-chooser");
+    divWaiting.style.display = "flex";
+    let divConnect4 = document.getElementById("wrap");
+    divConnect4.style.display = "none";
+    let divOpponentDisconnected = document.getElementById("overlay");
+    divOpponentDisconnected.style.display = "none";
+  });
+
+  function updateLoadingText() {
+    const loadingElement = document.getElementById('loadingText');
+    let loadingText = loadingElement.textContent;
+    let dotCount = (loadingText.match(/\./g) || []).length;
+  
+    if (dotCount < 3) {
+      loadingElement.textContent = 'Waiting for players' + '.'.repeat(dotCount + 1);
+    } else {
+      loadingElement.textContent = 'Waiting for players.';
+    }
+  
+  }
+  
+  let loadingText = setInterval(updateLoadingText, 500);
+
   document.addEventListener('htmx:beforeSwap', function(event) {
     /* TODO remove all event listeners here*/
     matchMakingSocket.close();
     console.log("htmx:beforeSwap event listener matchMakingSocket close");
     heartbeat = clearInterval(heartbeat);
+    clearInterval(loadingText);
   }, {once: true});
 }
 
-function updateLoadingText() {
-  const loadingElement = document.getElementById('loadingText');
-  let loadingText = loadingElement.textContent;
-  let dotCount = (loadingText.match(/\./g) || []).length;
-
-  if (dotCount < 3) {
-    loadingElement.textContent = 'Waiting for players' + '.'.repeat(dotCount + 1);
-  } else {
-    loadingElement.textContent = 'Waiting for players.';
-  }
-}
-
-let loadingText = setInterval(updateLoadingText, 500);
 
 
-document.getElementById("connect4-button").addEventListener("click", function() {
-  console.log("connect4-button clicked");
-  createSocket("connect4");
-  let divWaiting = document.getElementById("game-chooser");
-  let divConnect4 = document.getElementById("wrap");
-  const playerDiv = document.getElementById("player-btn");
-  const opponentDiv = document.getElementById("opps-btn");
-  const gameDiv = document.getElementById("game-type");
-  const vsDiv = document.getElementById("vs-text");
-  const waitingDiv = document.getElementById("waiting-btn");
-  [playerDiv, opponentDiv, gameDiv, vsDiv, divWaiting].forEach(el => el.style.display = "none");
-  [divConnect4, waitingDiv].forEach(el => el.style.display = "flex");
-});
-
-document.getElementById("pong-button").addEventListener("click", function() {
-  console.log("pong-button clicked");
-  createSocket("pong");
-  let divWaiting = document.getElementById("game-chooser");
-  let divConnect4 = document.getElementById("wrap");
-  const playerDiv = document.getElementById("player-btn");
-  const opponentDiv = document.getElementById("opps-btn");
-  const gameDiv = document.getElementById("game-type");
-  const vsDiv = document.getElementById("vs-text");
-  const waitingDiv = document.getElementById("waiting-btn");
-  [playerDiv, opponentDiv, gameDiv, vsDiv, divWaiting].forEach(el => el.style.display = "none");
-  [divConnect4, waitingDiv].forEach(el => el.style.display = "flex");
-});
-
-document.getElementById("reconnect").addEventListener("click", function() {
-  console.log("reconnect clicked");
-  createSocket();
-  const playerDiv = document.getElementById("player-btn");
-  const opponentDiv = document.getElementById("opps-btn");
-  const gameDiv = document.getElementById("game-type");
-  const vsDiv = document.getElementById("vs-text");
-  const waitingDiv = document.getElementById("waiting-btn");
-  const divOpponentDisconnected = document.getElementById("overlay");
-  const divConnect4 = document.getElementById("wrap");
-  [playerDiv, opponentDiv, gameDiv, vsDiv, divOpponentDisconnected].forEach(el => el.style.display = "none");
-  [divConnect4, waitingDiv].forEach(el => el.style.display = "flex");
-});
-
-document.getElementById("cancel").addEventListener("click", function() {
-  console.log("cancel clicked");
-  let divWaiting = document.getElementById("game-chooser");
-  divWaiting.style.display = "flex";
-  let divConnect4 = document.getElementById("wrap");
-  divConnect4.style.display = "none";
-  let divOpponentDisconnected = document.getElementById("overlay");
-  divOpponentDisconnected.style.display = "none";
-});
-
-document.addEventListener('htmx:beforeSwap', function(event) {
-  /* TODO remove all event listeners here*/
-  console.log("htmx:beforeSwap event listener clear interval");
-  clearInterval(loadingText);
-}, {once: true});
+export { createSocket };
