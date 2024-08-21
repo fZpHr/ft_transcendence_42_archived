675562c8f0fb06cf52cf4832b3743ca4310e8547
Ezuker <benoit.carolle.pro@gmail.com>
2024-08-21 14:19:15 -0400

    fix: profil.js loaded when spa

--- common/web/conf/transcendence/settings.py
+++ common/web/conf/transcendence/settings.py
@@ -194,9 +194,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
