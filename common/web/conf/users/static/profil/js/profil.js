54ae89fa20cb98248d5eb86aae4c3125852934ee
Ezuker <benoit.carolle.pro@gmail.com>
2024-09-05 05:40:50 -0400

    fix: progress is now SPA fix chat too

--- common/web/conf/users/static/profil/js/profil.js
+++ common/web/conf/users/static/profil/js/profil.js
@@ -293,6 +293,17 @@ async function innerShowProgress(games, type) {
         let href = (type === 'connect4' ? '/progress/connect4' : '/progress/pong');
 		console.log(href);
 		progressButton.innerHTML = `<a id="progress-link" hx-get="${href}" hx-target="#main-content" hx-push-url="true" href="${href}" class="btn">Show My Progress</a>`
+		let progressLink = document.getElementById('progress-link');
+		progressLink.addEventListener('click', async (e) => {
+			e.preventDefault();
+			let href = e.currentTarget.href;
+			htmx.ajax('GET', href, {
+				target: '#main-content', // The target element to update
+				swap: 'innerHTML', // How to swap the content
+			}).then(response => {
+				history.pushState({}, '', href);
+			});
+		});
 	} catch (e) {
 		console.error(e);
 	}
